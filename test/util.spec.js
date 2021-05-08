2edeab1dae191d4bc7b0bf6b5172cd6822eabb18
Bernhard Pottler <bpottler@nexgo.de>
2021-05-08 11:19:30 +0200

    refactor: use default export for util.js in util.spec.js

--- test/util.spec.js
+++ test/util.spec.js
@@ -7,26 +7,26 @@ import chai from 'chai';
 import { expect } from 'chai';
 /* eslint-enable node/no-unpublished-import */
 
-import { licenseFileName, licenseFilesTargetFolder, sourceToTargetFilename } from '../lib/util.js';
+import util from '../lib/util.js';
 
 describe('util', () => {
   describe('sourceToTargetFilename without targetDirectory', () => {
     it('should generate target filename from absolute path', () => {
-      const target = sourceToTargetFilename('/absolute/path/to/sourceToTargetFilename.json');
+      const target = util.sourceToTargetFilename('/absolute/path/to/sourceToTargetFilename.json');
       const expectedTarget = path.join('/absolute/path/to/sourceToTargetFilename.ext.json');
 
       expect(target).to.equal(expectedTarget);
     });
 
     it('should generate target filename from relative path', () => {
-      const target = sourceToTargetFilename('relative/path/to/sourceToTargetFilename.json');
+      const target = util.sourceToTargetFilename('relative/path/to/sourceToTargetFilename.json');
       const expectedTarget = path.join('relative/path/to/sourceToTargetFilename.ext.json');
 
       expect(target).to.equal(expectedTarget);
     });
 
     it('should throw generating target filename from empty string', () => {
-      const generateFilenameFromEMptyString = () => { sourceToTargetFilename(''); };
+      const generateFilenameFromEMptyString = () => { util.sourceToTargetFilename(''); };
 
       expect(generateFilenameFromEMptyString, 'Generating target filename from empty string should throw').to.throw();
     });
@@ -34,21 +34,21 @@ describe('util', () => {
 
   describe('sourceToTargetFilename with targetDirectory', () => {
     it('should generate target filename from absolute path', () => {
-      const target = sourceToTargetFilename('/absolute/path/to/sourceToTargetFilename.json', '/target/path');
+      const target = util.sourceToTargetFilename('/absolute/path/to/sourceToTargetFilename.json', '/target/path');
       const expectedTarget = path.join('/target/path', 'sourceToTargetFilename.ext.json');
 
       expect(target).to.equal(expectedTarget);
     });
 
     it('should generate target filename from relative path', () => {
-      const target = sourceToTargetFilename('relative/path/to/sourceToTargetFilename.json', 'target/path');
+      const target = util.sourceToTargetFilename('relative/path/to/sourceToTargetFilename.json', 'target/path');
       const expectedTarget = path.join('target/path', 'sourceToTargetFilename.ext.json');
 
       expect(target).to.equal(expectedTarget);
     });
 
     it('should throw generating target filename from empty string', () => {
-      const generateFilenameFromEMptyString = () => { sourceToTargetFilename('', ''); };
+      const generateFilenameFromEMptyString = () => { util.sourceToTargetFilename('', ''); };
 
       expect(generateFilenameFromEMptyString, 'Generating target filename from empty string should throw').to.throw();
     });
@@ -58,7 +58,7 @@ describe('util', () => {
     it('should return entered licenseDirectory', () => {
       const licenseDirectory = '/tmp/downloadDir';
       const sourceDir = undefined;
-      const dir = licenseFilesTargetFolder(licenseDirectory, sourceDir);
+      const dir = util.licenseFilesTargetFolder(licenseDirectory, sourceDir);
 
       expect(dir).to.equal(licenseDirectory);
     });
@@ -66,7 +66,7 @@ describe('util', () => {
     it('should return folder in sourceDirectory if licenseDirectory is undefined', () => {
       const licenseDirectory = undefined;
       const sourceDir = '/path/of/source/json';
-      const dir = licenseFilesTargetFolder(licenseDirectory, sourceDir);
+      const dir = util.licenseFilesTargetFolder(licenseDirectory, sourceDir);
 
       expect(dir).to.equal(path.join(sourceDir, 'licenses'));
     });
@@ -74,7 +74,7 @@ describe('util', () => {
     it('should return folder "licenses" if no parameters are given', () => {
       const licenseDirectory = undefined;
       const sourceDir = undefined;
-      const dir = licenseFilesTargetFolder(licenseDirectory, sourceDir);
+      const dir = util.licenseFilesTargetFolder(licenseDirectory, sourceDir);
 
       expect(dir).to.equal('licenses');
     });
@@ -82,14 +82,14 @@ describe('util', () => {
 
   describe('licenseFileName', () => {
     it('should return filename with version', () => {
-      const fileNameComponents = licenseFileName('package', '1.2.3');
+      const fileNameComponents = util.licenseFileName('package', '1.2.3');
 
       expect(fileNameComponents.scope).to.equal('');
       expect(fileNameComponents.packageName).to.equal('package@1.2.3');
     });
 
     it('should return scope and filename with version', () => {
-      const fileNameComponents = licenseFileName('@test/package-test', '2.3.4');
+      const fileNameComponents = util.licenseFileName('@test/package-test', '2.3.4');
 
       expect(fileNameComponents.scope).to.equal('@test');
       expect(fileNameComponents.packageName).to.equal('package-test@2.3.4');
