cb020408288e857f8dab5802fbbe7d192e0e0477
Bernhard Pottler <bpottler@nexgo.de>
2024-06-30 14:51:15 +0200

    feat: add --help flag for cli to show short documentation

--- index.js
+++ index.js
@@ -5,19 +5,24 @@ import path from 'path';
 import config from './lib/config.js';
 import FsService from './lib/filesystem-service.js';
 import WebService from './lib/web-service.js';
-import util from './lib/util.js';
+import { helpText, licenseFilesTargetFolder, sourceToTargetFilename } from './lib/util.js';
 
 (async () => {
+  if (config.help) {
+    console.log(helpText); // eslint-disable-line security-node/detect-crlf
+    return;
+  }
+
   try {
     const sourceFilename = config.source;
     const json = await FsService.readJson(sourceFilename);
     await WebService.addLicenseFilePath(json, config.httpRetryOptions, config.githubToken);
-    const targetFilename = util.sourceToTargetFilename(sourceFilename, config.licDir);
+    const targetFilename = sourceToTargetFilename(sourceFilename, config.licDir);
     FsService.createDirFromFilename(targetFilename);
     await FsService.writeJson(json, targetFilename);
     if (config.download) {
       const sourceDirectory = path.dirname(sourceFilename);
-      const targetDirectory = util.licenseFilesTargetFolder(config.licDir, sourceDirectory);
+      const targetDirectory = licenseFilesTargetFolder(config.licDir, sourceDirectory);
       await WebService.downloadLicenseFiles(json, targetDirectory, config.httpRetryOptions, config.githubToken);
     }
   } catch (e) {
